3ebb9283ede141f90ceeda78122fbafe8444e0de
Markus Kummer <52301628@edu.campus02.at>
2024-01-27 08:27:46 +0100

    Initial commit

---
+++ src/main/java/BMICalculator.java
@@ -0,0 +1,85 @@
+import static java.lang.Math.round;
+
+public class BMICalculator {
+    private String firstname;
+    private String lastname;
+    private int bodyHeight;
+    private double bodyWeight;
+    private char gender;
+
+    public BMICalculator(String firstname, String lastname, int bodyHeight, double bodyWeight, char gender) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.bodyHeight = bodyHeight;
+        this.bodyWeight = bodyWeight;
+        this.gender = gender;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getBodyHeight() {
+        return bodyHeight;
+    }
+
+    public double getBodyWeight() {
+        return bodyWeight;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setBodyHeight(int bodyHeight) {
+        this.bodyHeight = bodyHeight;
+    }
+
+    public void setBodyWeight(double bodyWeight) {
+        this.bodyWeight = bodyWeight;
+    }
+
+    public double calculateBMI(){
+        return Math.round(100.00 * (bodyWeight / (((double) bodyHeight / 100) * ((double) bodyHeight / 100)))) / 100.00;
+    }
+
+    public int calculateBMICategory(){
+        double bmi = calculateBMI();
+
+        if (gender == 'm'){
+            if (bmi < 16.0) return -2;
+            else if (bmi < 18.5) return -1;
+            else if (bmi < 25.0) return 0;
+            else if (bmi < 35.0) return 1;
+            else if (bmi >= 35.0) return 2;
+        }
+        else{
+            if (bmi < 15.0) return -2;
+            else if (bmi < 17.5) return -1;
+            else if (bmi < 24.0) return 0;
+            else if (bmi < 34.0) return 1;
+            else if (bmi >= 34.0) return 2;
+        }
+        return -3;
+    }
+
+    public String getBMICategoryName(){
+        switch (calculateBMICategory()){
+            case -2:
+                return "Sehr starkes Untergewicht";
+            case -1:
+                return "Untergewicht";
+            case 0:
+                return "Normalgewicht";
+            case 1:
+                return "Übergewicht";
+            case 2:
+                return "Sehr starkes Übergewicht";
+        }
+        return "nicht kategorisiert";
+    }
+}
